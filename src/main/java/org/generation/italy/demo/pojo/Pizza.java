c1e38c98065f668a3893daa675142a56bef1f573
Antonio Mendetta <Antonio@MBP-di-Antonio.station>
2022-12-16 15:55:14 +0100

    create Pizza entity and link project to DB

---
+++ src/main/java/org/generation/italy/demo/pojo/Pizza.java
@@ -0,0 +1,43 @@
+package org.generation.italy.demo.pojo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table
+public class Pizza {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@NotNull
+	@Column
+	private String name;
+	
+	public Pizza(String name) {
+		
+		setName(name);
+	}
+
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
